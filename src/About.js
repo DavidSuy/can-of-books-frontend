3bb3ca67568a1669f9719ab8f5bcec2ad2b60b79
David Suy <DavidSuyDev@gmail.com>
2022-08-29 17:33:41 -0700

    add carousel

--- src/About.js
+++ src/About.js
@@ -1,11 +1,26 @@
 import { Component } from "react";
 
 class Profile extends Component {
-
   render() {
     /* TODO: render information about the developers */
-    return <p>Profile page coming soon</p>
+    return (
+      <>
+        <h1>About Us</h1>
+        <h2>David Suy</h2>
+        <p>
+          David Suy is Full Stack Web Developer with background in Aviation from
+          the Marine Corps. Currently resides in Seattle WA.
+        </p>
+        <h2>Jackson Gurney</h2>
+        <p>
+          Jackson Gurney is a Software Developer located in Seattle, Wa. He
+          currently enjoys coding in javascript, learning React, bootstrap and
+          databases. In his free time he enjoys playing Dota 2 and hanging out
+          with his girlfriend. He is survived by his dog Hugo.
+        </p>
+      </>
+    );
   }
-};
+}
 
 export default Profile;
--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React from "react";
+import axios from "axios";
+import BooksCarousel from "./Carousel";
+let SERVER = process.env.REACT_APP_SERVER;
 
 class BestBooks extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      books: []
-    }
+      books: [],
+    };
   }
 
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
+  getBooks = async () => {
+    try {
+      let results = await axios.get(`${SERVER}/books`);
+      this.setState({
+        books: results.data,
+      });
+    } catch (err) {
+      console.log(`We hava an error: ${err.response.data}`);
+    }
+  };
+
+  componentDidMount() {
+    this.getBooks();
+  }
 
   render() {
-
     /* TODO: render all the books in a Carousel */
 
     return (
@@ -19,12 +35,13 @@ class BestBooks extends React.Component {
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
 
         {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
+          <BooksCarousel books={this.state.books} />
         ) : (
+          // <p>Book Carousel coming soon</p>
           <h3>No Books Found :(</h3>
         )}
       </>
-    )
+    );
   }
 }
 
