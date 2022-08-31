84e9055785f63b73485d793a6907cb417910748e
David Suy <DavidSuyDev@gmail.com>
2022-08-30 21:21:17 -0700

    add delete book feature

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -2,17 +2,16 @@ import React from "react";
 import Carousel from "react-bootstrap/Carousel";
 import axios from "axios";
 import BookForm from "./BookFormModal";
-
+import { Button } from "react-bootstrap";
 
 class BestBooks extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       books: [],
-      hasBooks: false
+      hasBooks: false,
     };
   }
-
 
   getBooks = async () => {
     let SERVER = process.env.REACT_APP_SERVER;
@@ -20,7 +19,7 @@ class BestBooks extends React.Component {
       let results = await axios.get(`${SERVER}/books`);
       this.setState({
         books: results.data,
-        hasBooks: true
+        hasBooks: true,
       });
     } catch (err) {
       console.log(`We have an error: ${err.message}`);
@@ -32,23 +31,32 @@ class BestBooks extends React.Component {
     let newBook = {
       title: e.target.title.value,
       description: e.target.description.value,
-      status: e.target.status.checked
-    }
+      status: e.target.status.checked,
+    };
     this.postBook(newBook);
-  }
+  };
   postBook = async (book) => {
     let url = `${process.env.REACT_APP_SERVER}/books`;
     let results = await axios.post(url, book);
-    console.log(results.data);
     this.setState({
-      books: [...this.state.books, results.data]
+      books: [...this.state.books, results.data],
     });
     this.getBooks();
-  }
+  };
+
+  handleDelete = async (book) => {
+    let url = `${process.env.REACT_APP_SERVER}/books/${book._id}`;
+
+    let result = await axios.delete(url);
+
+    this.setState({
+      books: this.state.books.filter((r) => r._id !== result.data._id),
+    });
+  };
+
   componentDidMount() {
     this.getBooks();
   }
-
 
   render() {
     let bookArr = this.state.books.map((book, idx) => {
@@ -63,6 +71,7 @@ class BestBooks extends React.Component {
             <h3>{book.title}</h3>
             <p>{book.description}</p>
             {book.status ? "❤️" : "💔"}
+            <Button onClick={() => this.handleDelete(book)}>Delete Book</Button>
           </Carousel.Caption>
         </Carousel.Item>
       );
@@ -70,8 +79,12 @@ class BestBooks extends React.Component {
     return (
       <>
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
-        {this.state.hasBooks ? (<Carousel>{bookArr}</Carousel>) : <h1>Waiting for Books</h1>}
-        <BookForm handlePost={this.handlePost}/>
+        {this.state.hasBooks ? (
+          <Carousel>{bookArr}</Carousel>
+        ) : (
+          <h1>Waiting for Books</h1>
+        )}
+        <BookForm handlePost={this.handlePost} />
       </>
     );
   }
